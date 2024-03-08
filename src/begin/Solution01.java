0f706a0803a46d77211897ab113cfcdf165cb56b
Diyorbek-Java <diorbeckturdiev@gmail.com>
2024-03-08 13:13:15 -0800

    Solution01 created

--- src/begin/Solution01.java
+++ src/begin/Solution01.java
@@ -1,7 +1,13 @@
 package begin;
 
+import java.util.Scanner;
+
 public class Solution01 {
     public static void main(String[] args) {
-
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Please enter the side of a Square:  ");
+        double aSide=scanner.nextInt();
+        double perimeter=aSide*4;
+        System.out.printf("Perimeter of the square with side=%s is %s",aSide,perimeter);
     }
 }
